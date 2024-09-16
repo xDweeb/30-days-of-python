7b6a1a4364deb32ee0a1077a79362f864ab19e88
xdweb <xdweb.elyakouti@gmail.com>
2024-09-17 00:08:44 +0100

    Exercice 4

--- Day-1/main.py
+++ Day-1/main.py
@@ -6,9 +6,32 @@ print("Entrez votre âge:")
 age = input()
 print(f"Bienvenue {nom}, vous avez {age} ans")
 
+#Exercice 2
 
+print("Entrez le premier nombre:")
+a = int(input())  # Convertir en entier
+print("Entrez le deuxième nombre:")
+b = int(input())  
 
+print("Somme:", a + b)
+print("Différence:", a - b)
+print("Produit:", a * b)
+print("Division:", a / b)
+print("Modulo:", a % b)
 
+#Exercice 3
+import random
+
+print("Entrez un nombre entre 1 et 100:")
+nombre = int(input())  
+nombre_aleatoire = random.randint(1, 100)  
+print(f"Nombre généré aléatoirement: {nombre_aleatoire}")
+
+#Exercice 4
+
+print("Entrez une valeur:")
+valeur = input()
+print("Le type de la valeur est:", type(valeur))
 
 
 '''
