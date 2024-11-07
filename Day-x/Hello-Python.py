1624ed97ac6debad381ae18b7198ee7187d1abd3
xdweb <xdweb.elyakouti@gmail.com>
2024-11-08 00:01:44 +0100

    by @xDweeb

--- Day-x/Hello-Python.py
+++ Day-x/Hello-Python.py
@@ -2,3 +2,12 @@
 # (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
 
 color = "Blue"
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+raduis = diameter / 2
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * raduis ** 2
