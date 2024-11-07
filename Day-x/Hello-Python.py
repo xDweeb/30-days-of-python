4d69916e630275eb90fcac9009ce6eedd93c8e5c
xdweb <xdweb.elyakouti@gmail.com>
2024-11-08 00:09:11 +0100

    by @xDweeb

--- Day-x/Hello-Python.py
+++ Day-x/Hello-Python.py
@@ -11,3 +11,31 @@ raduis = diameter / 2
 
 # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
 area = pi * raduis ** 2
+
+########### Setup code - don't touch this part ######################
+# If you're curious, these are examples of lists. We'll talk about 
+# them in depth a few lessons from now. For now, just know that they're
+# yet another type of Python object, like int or float.
+a = [1, 2, 3]
+b = [3, 2, 1]
+q2.store_original_ids()
+######################################################################
+
+# Your code goes here. Swap the values to which a and b refer.
+# If you get stuck, you can always uncomment one or both of the lines in
+# the next cell for a hint, or to peek at the solution.
+tmp = a
+a = b
+b = tmp
+######################################################################
+
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
+
+
